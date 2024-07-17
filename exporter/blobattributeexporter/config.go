51e23bec04ddbcd1404356b8bb5a2c0ba51c3ad7
Michael Aaron Safyan <michaelsafyan@google.com>
2024-07-17 11:24:30 -0400

    Start working on validation.

--- exporter/blobattributeexporter/config.go
+++ exporter/blobattributeexporter/config.go
@@ -116,3 +116,108 @@ type MetadataLabelValueConfig struct {
 	// Allows the label value to be extracted from other properties.
 	Extraction ExtractionConfig `mapstructure:"extraction"`
 }
+
+// Verifies that the configuration is valid.
+func (c *Config) Validate() error {
+	if err := c.TracesConfig.Validate() ; err != nil {
+		return err
+	}
+	return nil
+}
+
+// Verifies that the configuration is valid.
+func (tc *TracesConfig) Validate() error {
+	if err := c.AttributeConfig.Validate() ; err != nil {
+		return err
+	}
+	return nil
+}
+
+// Helper for "TraceAttributeConfig.Validate()" below.
+func validateTraceLocation(ruleName string, location string) error {
+	if location == "span" {
+		return nil
+	}
+	if location == "scope" || location == "instrumentationScope" || location == "instrumentation_scope" {
+		return nil
+	}
+	if location == "resource" {
+		return nil
+	}
+	return fmt.Errorf("In rule %v: unknown location: %v. Valid values are: [span, scope, resource].", ruleName, location)
+}
+
+// Verifies that the configuration is valid.
+func (tac *TraceAttributeConfig) Validate() error {
+	for _, rule := range tac.Rule {
+		if err := rule.Validate() ; err != nil {
+			return err
+		}
+		for _, location := range rule.Match.Locations {
+			if err := validateTraceLocation(rule.name, location) ; err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// Verifies that the configuration is valid.
+func (acr *AttributeConfigRule) Validate() error {
+	// TODO: implement
+	return nil
+}
+
+// Verifies that the configuration is valid.
+func (mc *MatchConfig) Validate() error {
+	// TODO: implement
+	return nil
+}
+
+// Verifies that the configuration is valid.
+func (ac *ActionConfig) Validate() error {
+	// TODO: implement
+	return nil
+}
+
+// Verifies that the configuration is valid.
+func (sc *SamplingConfig) Validate() error {
+	// TODO: implement
+	return nil
+}
+
+// Verifies that the configuration is valid.
+func (uc *UploadConfig) Validate() error {
+	// TODO: implement
+	return nil
+}
+
+// Verifies that the configuration is valid.
+func (ctc *ContentTypeConfig) Validate() error {
+	// TODO: implement
+	return nil
+}
+
+// Verifies that the configuration is valid.
+func (actc *AutoContentTypeConfig) Validate() error {
+	// TODO: implement
+	return nil
+}
+
+// Verifies that the configuration is valid.
+func (ec *ExtractionConfig) Validate() error {
+	// TODO: implement
+	return nil
+}
+
+// Verifies that the configuration is valid.
+func (mlc *MetadataLabelConfig) Validate() error {
+	// TODO: implement
+	return nil
+}
+
+// Verifies that the configuration is valid.
+func (mlvc *MetadataLabelValueConfig) Validate() error {
+	// TODO: implement
+	return nil
+}
