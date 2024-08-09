9f2070d5a11918da597c625015433c0296f6282b
Michael Aaron Safyan <michaelsafyan@google.com>
2024-08-09 12:47:06 -0400

    Checkpoint current state.

--- connector/blobattributeuploadconnector/traces.go
+++ connector/blobattributeuploadconnector/traces.go
@@ -9,6 +9,9 @@ package blobattributeuploadconnector
 
 import (
 	"context"
+	"hash/maphash"
+
+	"go.uber.org/zap"
 
 	"go.opentelemetry.io/collector/component"
 	"go.opentelemetry.io/collector/connector"
@@ -42,11 +45,19 @@ type spanReference struct {
 	ottlCtx       ottlspan.TransformContext
 }
 
+func (s *spanReference) traceID() pcommon.TraceID {
+	return s.span.TraceID()
+}
+
 type spanEventReference struct {
 	span    *spanReference
 	index   int
 	event   ptrace.SpanEvent
 	ottlCtx ottlspanevent.TransformContext
+}
+
+func (se *spanEventReference) traceID() pcommon.TraceID {
+	return se.span.traceID()
 }
 
 type attributeRuleMap struct {
@@ -181,12 +192,197 @@ type tracesToTracesImpl struct {
 	backendRegistry backend.Registry
 	spanFuncs map[string]ottl.Factory[ottlspan.TransformContext]
 	spanEventFuncs map[string]ottl.Factory[ottlspanevent.TransformContext]
+	seed maphash.Seed
 	component.StartFunc
 	component.ShutdownFunc
 }
 
+func setInMap(m pcommon.Map, key string, item pcommon.Value) {
+	item.CopyTo(m.PutEmpty(key))
+}
+
+func (tracesImpl *tracesToTracesImpl) shouldSampleAttributeInTrace(m *matchedAttribute, traceID pcommon.TraceID) bool {
+	if m.rule.Action == nil  {
+		return false
+	}
+	if m.rule.Action.Sampling == nil {
+		return true
+	}
+	if m.rule.Action.Sampling.Enabled == false {
+		return true
+	}
+
+	samplePercent := m.rule.Action.Sampling.Percent
+	hashVal := maphash.Bytes(tracesImpl.seed, traceID)
+	mod100 := hashVal % 100
+	return mod100 < samplePercent
+}
+
+func (tracesImpl *tracesToTracesImpl) shouldSampleSpanEventAttribute(se *spanEventReference, m *matchedAttribute) bool {
+	return tracesImpl.shouldSampleAttributeInTrace(m, se.traceID()) 
+}
+
+func (tracesImpl *tracesToTracesImpl) shouldSampleSpanAttribute(s *spanReference, m *matchedAttribute) bool {
+	return tracesImpl.shouldSampleAttributeInTrace(m, s.traceID()) 
+}
+
+func (tracesImpl *tracesToTracesImpl) computeDestinationUriForSpanEvent(
+	ctx context.Context,
+	se *spanEventReference,
+    m *matchedAttribute) (string, error) {
+  // TODO: ...
+}
+
+func (tracesImpl *tracesToTracesImpl) computeDestinationUriForSpan(
+	ctx context.Context,
+	s *spanReference,
+    m *matchedAttribute) (string, error) {
+  // TODO: ...
+}
+
+func computeDataEncoding(value pcommon.Value) ([]byte, error) {
+  // TODO: ...
+}
+
+func (tracesImpl *tracesToTracesImpl) computeContentTypeForSpanEventAttribute(
+	ctx context.Context,
+	se *spanEventReference,
+	m *matchedAttribute,
+	data []byte) (string, error) {
+ // TODO: ...
+}
+
+func (tracesImpl *tracesToTracesImpl) computeContentTypeForSpanAttribute(
+	ctx context.Context,
+	s *spanReference,
+	m *matchedAttribute,
+	data []byte) (string, error) {
+  // TODO: ...
+}
+
+func (tracesImpl *tracesToTracesImpl) computeUploadMetadataForSpanEvent(
+	ctx context.Context,
+	se *spanEventReference,
+    m *matchedAttribute) (map[string]string, error) {
+  // TODO: ...
+}
+
+func (tracesImpl *tracesToTracesImpl) computeUploadMetadataForSpan(
+	ctx context.Context,
+	s *spanReference,
+    m *matchedAttribute) (map[string]string, error) {
+  // TODO: ...
+}
+
+type pendingUpload struct {
+	storageBackend backend.BlobStorageBackend
+	key string
+	data []byte
+	destinationUri string
+	contentType string
+	metadataLabels map[string]string
+}
+
+func (tracesImpl *tracesToTracesImpl) scheduleUpload(
+	ctx context.Context,
+	pending *pendingUpload) error {
+  // TODO: ...
+}
+
+func (tracesImpl *tracesToTracesImpl) createForeignAttr(uri string, contentType string) pcommon.Value {
+	if len(contentType) == 0 {
+		return foreignattr.FromUri(uri)
+	}
+	return foreignattr.FromUriWithContentType(uri, contentType)
+}
+
+func (tracesImpl *tracesToTracesImpl) processSingleMatchedSpanEventAttribute(
+	ctx context.Context,
+	se *spanEventReference,
+    m *matchedAttribute) (pcommon.Value, error) {
+	if (!tracesImpl.shouldSampleSpanEventAttribute(se, m)) {
+		return nil, nil
+	}
+
+	destinationUri, destinationUriErr := tracesImpl.computeDestinationUriForSpanEvent(ctx, se, m)
+	if destinationUriErr != nil {
+		tracesImpl.settings.Logger.Error(
+			"Could not create destination URI for span event attribute",
+			zap.String("attributeKey", m.key),
+			zap.String("configRuleName", m.rule.Name),
+		    zap.NamedError("error", destinationUriErr))
+		return nil, destinationUriErr
+	}
+
+	b, berr := tracesImpl.backendRegistry.GetBackendForUri(destinationUri)
+	if berr != nil {
+		tracesImpl.settings.Logger.Error(
+			"Could not find suitable storage backend for destination URI",
+			zap.String("destinationUri", destinationUri),
+		    zap.NamedError("error", berr))
+		return nil, berr
+	}
+
+	d, derr := computeDataEncoding(m.value)
+	if derr != nil {
+		return nil, derr
+	}
+
+	contentType, contentTypeErr := tracesToTracesImpl.computeContentTypeForSpanEventAttribute(ctx, se, m, d)
+	if contentTypeErr != nil {
+		return nil, contentTypeErr
+	}
+
+	pending := &pendingUpload{
+		storageBackend: b,
+		key: m.key,
+		data: d,
+		destinationUri: destinationUri,
+		contentType: contentType,
+	}
+
+	err := tracesImpl.scheduleUpload(ctx, pending)
+	if err != nil {
+		return nil, err
+	}
+
+	return tracesImpl.createForeignAttr(destinationUri, contentType), nil
+}
+
 func (tracesImpl *tracesToTracesImpl) consumeSpanEvent(ctx context.Context, se *spanEventReference) error {
-	// TODO: ...
+	name := se.event.Name()
+	rulesResolver := tracesImpl.spanEvents.get(name)
+	m := se.event.Attributes()
+	toProcess := newMatchedAttributeList()
+	m.Range(func(k string, v pcommon.Value) bool { 
+		rule := rulesResolver.get(k)
+		if rule == nil {
+			return true
+		}
+
+		matchedAttr := &matchedAttribute{
+			key: k,
+			value: v,
+			rule: rule,
+		}
+
+		tracesImpl.settings.Logger.Debug("Found matching span event attribute", zap.String("attributeKey", k))
+		toProcess.add(matchedAttr)
+		return true
+	})
+
+	for _, entry := toProcess.items {
+		newVal, err := tracesImpl.processSingleMatchedSpanEventAttribute(ctx, se, entry)
+		if err != nil {
+			return err
+		}
+
+		m.Remove(entry.key)
+		if newVal != nil {
+			setInMap(m, entry.key, newVal)
+		}
+	}
+
 	return nil
 }
 
@@ -194,8 +390,53 @@ func (tracesImpl *tracesToTracesImpl) processSingleMatchedSpanAttribute(
 	ctx context.Context,
 	s *spanReference,
     m *matchedAttribute) (pcommon.Value, error) {
-	// TODO: ...
-	return nil, nil
+	if (!tracesImpl.shouldSampleSpanAttribute(s, m)) {
+		return nil, nil
+	}
+
+	destinationUri, destinationUriErr := tracesImpl.computeDestinationUriForSpan(ctx, s, m)
+	if destinationUriErr != nil {
+		tracesImpl.settings.Logger.Error(
+			"Could not create destination URI for span attribute",
+			zap.String("attributeKey", m.key),
+			zap.String("configRuleName", m.rule.Name),
+		    zap.NamedError("error", destinationUriErr))
+		return nil, destinationUriErr
+	}
+
+	b, berr := tracesImpl.backendRegistry.GetBackendForUri(destinationUri)
+	if berr != nil {
+		tracesImpl.settings.Logger.Error(
+			"Could not find suitable storage backend for destination URI",
+			zap.String("destinationUri", destinationUri),
+		    zap.NamedError("error", berr))
+		return nil, berr
+	}
+
+	d, derr := computeDataEncoding(m.value)
+	if derr != nil {
+		return nil, derr
+	}
+
+	contentType, contentTypeErr := tracesToTracesImpl.computeContentTypeForSpanAttribute(ctx, s, m, d)
+	if contentTypeErr != nil {
+		return nil, contentTypeErr
+	}
+
+	pending := &pendingUpload{
+		storageBackend: b,
+		key: m.key,
+		data: d,
+		destinationUri: destinationUri,
+		contentType: contentType,
+	}
+
+	err := tracesImpl.scheduleUpload(ctx, pending)
+	if err != nil {
+		return nil, err
+	}
+
+	return tracesImpl.createForeignAttr(destinationUri, contentType), nil
 }
 
 func (tracesImpl *tracesToTracesImpl) consumeSpanContent(ctx context.Context, s *spanReference) error {
@@ -214,6 +455,7 @@ func (tracesImpl *tracesToTracesImpl) consumeSpanContent(ctx context.Context, s 
 		}
 
 		toProcess.add(matchedAttr)
+		tracesImpl.settings.Logger.Debug("Found matching span attribute", zap.String("attributeKey", k))
 		return true
 	})
 
@@ -235,6 +477,12 @@ func (tracesImpl *tracesToTracesImpl) consumeSpanContent(ctx context.Context, s 
 func (tracesImpl *tracesToTracesImpl) consumeSpan(ctx context.Context, s *spanReference) error {
 	for i := 0; i < s.span.Events().Len(); i++ {
 		event := s.span.Events().At(i)
+		tracesImpl.settings.Logger.Debug(
+			"Processing span event",
+			zap.String("traceID", s.span.TraceID().String()),
+			zap.String("spanID", s.span.SpanID().String()),
+			zap.Int("eventIndex", i),
+		    zap.String("eventName", event.Name()))
 		ottlCtx := ottlspanevent.NewTransformContext(
 			event,
 			s.span,
@@ -260,6 +508,8 @@ func (tracesImpl *tracesToTracesImpl) Capabilities() consumer.Capabilities {
 }
 
 func (tracesImpl *tracesToTracesImpl) ConsumeTraces(ctx context.Context, td ptrace.Traces) error {
+	tracesImpl.settings.Logger.Debug("Received new traces batch to process")
+
 	resourceSpans := td.ResourceSpans()
 	for i := 0; i < resourceSpans.Len(); i++ {
 		resourceSpan := resourceSpans.At(i)
@@ -270,6 +520,13 @@ func (tracesImpl *tracesToTracesImpl) ConsumeTraces(ctx context.Context, td ptra
 			scope := scopeSpan.Scope()
 			for k := 0; k < scopeSpan.Spans().Len(); k++ {
 				span := scopeSpan.Spans().At(k)
+				tracesImpl.settings.Logger.Debug(
+					"Processing span",
+					zap.Int("resouceIndex", i),
+					zap.Int("scopeIndex", j),
+					zap.Int("spanIndex", k),
+					zap.String("traceID", span.TraceID().String()),
+				    zap.String("spanID", span.SpanID().String()))
 				ottlCtx := ottlspan.NewTransformContext(
 					span, scope, resource, scopeSpans, resourceSpans)
 				ref := &spanReference{
@@ -287,6 +544,7 @@ func (tracesImpl *tracesToTracesImpl) ConsumeTraces(ctx context.Context, td ptra
 		}
 	}
 
+	tracesImpl.settings.Logger.Debug("Forwarding processed batch to downstream consumer")
 	return tracesImpl.nextConsumer.ConsumeTraces(ctx, td)
 }
 
@@ -372,6 +630,7 @@ func createTracesToTracesConnector(
 	nextConsumer consumer.Traces) (connector.Traces, error) {
   cfg := config.(*Config)
   if cfg.Traces == nil {
+	  settings.Logger.Info("No trace configuration found; using pass-through connector.")
 	  return &passThroughTracesConnector{
 		  nextConsumer: nextConsumer,
 	  }, nil
@@ -379,18 +638,31 @@ func createTracesToTracesConnector(
 
   backendRegistry, backendRegistryErr := backend.NewRegistry()
   if backendRegistryErr != nil {
+	  settings.Logger.Error(
+		  "Failed to construct backend registry",
+		  zap.NamedError("error", backendRegistryErr))
 	  return nil, backendRegistryErr
   }
+  settings.Logger.Debug("Constructed backend registry")
 
   spanAttributes, spanAttributesErr := spanAttributesFromConfig(cfg)
   if spanAttributesErr != nil {
+	  settings.Logger.Error(
+		  "Failed to gather span attribute configuration",
+		  zap.NamedError("error", spanAttributesErr))
 	  return nil, spanAttributesErr
   }
+  settings.Logger.Debug("Collected span attribute configuration")
 
   spanEvents, spanEventsErr := spanEventAttributesFromConfig(cfg)
   if spanEventsErr != nil {
+	settings.Logger.Error(
+		"Failed to gather span event attribute configuration",
+		zap.NamedError("error", spanEventsErr))
 	  return nil, spanEventsErr
   }
+  settings.Logger.Debug("Collected span event attribute configuration")
+
 
   result := &tracesToTracesImpl{
 	  settings: settings,
@@ -400,5 +672,9 @@ func createTracesToTracesConnector(
 	  backendRegistry: backendRegistry,
 	  spanFuncs: createSpanFuncs(),
 	  spanEventFuncs: createSpanEventFuncs(),
+	  seed: maphash.MakeSeed(),
   }
+  settings.Logger.Debug("Constructed traces-to-traces connector")
+
+  return result, nil
 }
